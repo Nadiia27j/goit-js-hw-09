92acbe68c56545da7566b0f607799490d642ce99
Надія Ткач <nadiya77723@gmail.com>
2022-09-19 20:58:22 +0300

    редагує таску 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,10 +17,12 @@ function onFormSubmit(e) {
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
     .then(({ position, delay }) => {
-      Notiflix.Notify.success('✅ Fulfilled promise ${position} in ${delay}ms');
+      console.log(position);
+      console.log(delay);
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
     })
     .catch(({ position, delay }) => {
-      Notiflix.Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
     delay += step;
   }
@@ -31,7 +33,7 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
-    setTimeout(delay => {
+    setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay });
       } else {
