b9ed2bf2c6f2af490f24bcfa9f911f87b9cef371
Надія Ткач <nadiya77723@gmail.com>
2022-09-19 20:40:42 +0300

    додає додаткове завдання до таски 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
@@ -23,7 +24,7 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
       if(selectedDates[0] < currentDate) {
-        window.alert("Please choose a date in the future"); 
+        Notiflix.Notify.failure("Please choose a date in the future"); 
         refs.btnStart.setAttribute('disabled', true);
       }
 
